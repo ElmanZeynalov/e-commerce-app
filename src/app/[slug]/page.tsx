33ce86182b09d816b19d5dc739dc04874e2d0e56
elmanzeynalov <elman.zeynalov@gmail.com>
2024-07-11 15:24:52 +0400

    wix api error

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import ProductImages from "@/app/components/ProductImages";
+import CustomizeProducts from "@/app/components/CustomizeProducts";
+import Add from "@/app/components/Add";
 
 function SinglePage() {
   return (
@@ -9,7 +11,49 @@ function SinglePage() {
         <ProductImages />
       </div>
       {/*{text}*/}
-      <div className="w-full flex flex-col lg:w-1/2 gap-6">text</div>
+      <div className="w-full flex flex-col lg:w-1/2 gap-6">
+        <h1 className="text-4xl font-medium">Product Name</h1>
+        <p className="text-gray-500">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi,
+          voluptatum! Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Nobis, ratione?
+        </p>
+        <div className="h-[2px] bg-gray-100" />
+        <div className="flex items-center gap-4">
+          <h3 className="text-xl text-gray-500 line-through">49$</h3>
+          <h2 className="font-medium text-2xl">55$</h2>
+        </div>
+        <div className="h-[2px] bg-gray-100 " />
+        <CustomizeProducts />
+        <Add />
+        <div className="h-[2px] bg-gray-100" />
+        <div className="text-sm">
+          <h4 className="font-medium mb-4">Title</h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores
+            impedit nesciunt nihil numquam recusandae vero, voluptatibus. Amet
+            et repellendus saepe.
+          </p>
+        </div>
+
+        <div className="text-sm">
+          <h4 className="font-medium mb-4">Title</h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores
+            impedit nesciunt nihil numquam recusandae vero, voluptatibus. Amet
+            et repellendus saepe.
+          </p>
+        </div>
+
+        <div className="text-sm">
+          <h4 className="font-medium mb-4">Title</h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores
+            impedit nesciunt nihil numquam recusandae vero, voluptatibus. Amet
+            et repellendus saepe.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
